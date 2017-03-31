cafe88874c8ccd5d95b3045229f8d61a0d255132
Stepan Ivanov <aastralplane@gmail.com>
2017-03-31 22:46:15 +1100

    Исправления

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-let navMain = document.querySelector('.main-nav__items');
-let navToggle = document.querySelector('.main-nav__toggle');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav__items--closed')) {
-    navMain.classList.remove('main-nav__items--closed');
-    navMain.classList.add('main-nav__items--opened');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
-  } else {
-    navMain.classList.add('main-nav__items--closed');
-    navMain.classList.remove('main-nav__items--opened');
-    navToggle.classList.remove('main-nav__toggle--opened');
-    navToggle.classList.add('main-nav__toggle--closed');
-  }
-});
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
@@ -37,52 +20,54 @@ function initMap() {
   marker.setMap(map);
 }
 
-// class Slider {
-//   constructor({
-//     elem
-//   }) {
-//     this.elem = elem;
-//     this.slides = document.querySelector('.reviews__items');
-//     this.elems = document.querySelector('.reviews__slides');
-//     this.prev = document.querySelector('.reviews__controls--prev')
-//     this.next = document.querySelector('.reviews__controls--next');
-//
-//     this.width = parseInt(getComputedStyle(this.slides).width);
-//     this.position = 0;
-//
-//
-//     this.next.onclick = this.clickOnNext.bind(this);
-//     this.prev.onclick = this.clickOnPrev.bind(this);
-//   }
-//
-//
-//   clickOnNext(event) {
-//     this.slideNext();
-//   }
-//
-//   clickOnPrev(event) {
-//     this.slidePrev();
-//   }
-//
-//   slidePrev() {
-//     this.position += this.width;
-//     if (this.position > 0) {
-//       this.position = 0;
-//     }
-//     this.slides.style.marginLeft = this.position + 'px';
-//   }
-//
-//   slideNext() {
-//     this.position -= this.width;
-//     if (this.position <= this.width - parseInt(getComputedStyle(this.elems).width)) {
-//       this.position = this.width - parseInt(getComputedStyle(this.elems).width);
-//     }
-//     this.slides.style.marginLeft = this.position + 'px';
-//   }
-// }
-//
-// let sliderWrapper = document.querySelector('.reviews__container');
-//
-// let slider = new Slider({
-//   elem: sliderWrapper
-// });
+let navMain = document.querySelector('.main-nav__items');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav__items--closed')) {
+    navMain.classList.remove('main-nav__items--closed');
+    navMain.classList.add('main-nav__items--opened');
+    navToggle.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.add('main-nav__toggle--opened');
+  } else {
+    navMain.classList.add('main-nav__items--closed');
+    navMain.classList.remove('main-nav__items--opened');
+    navToggle.classList.remove('main-nav__toggle--opened');
+    navToggle.classList.add('main-nav__toggle--closed');
+  }
+});
+
+let indexCartBtn = document.querySelector('.popular-product__buy');
+let modalCart = document.querySelector('.modal-cart');
+let overlay = document.querySelector('.modal-cart__overlay');
+
+if (indexCartBtn) {
+  indexCartBtn.addEventListener('click', function(event) {
+    event.preventDefault('');
+    modalCart.classList.add('modal-cart--show');
+    overlay.classList.add('modal-cart--show');
+  });
+}
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+    modalCart.classList.remove('modal-cart--show');
+    overlay.classList.remove('modal-cart--show');
+  }
+});
+
+overlay.addEventListener('click', function(event) {
+  modalCart.classList.remove('modal-cart--show');
+});
+
+let cartBtns = document.querySelectorAll('.catalog-products__buy-button');
+
+if (cartBtns) {
+  for (let i = 0; i <= cartBtns.length; i++) {
+    cartBtns[i].addEventListener('click', function() {
+      event.preventDefault('');
+      modalCart.classList.add('modal-cart--show');
+      overlay.classList.add('modal-cart--show');
+    });
+  }
+}
